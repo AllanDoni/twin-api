758f603f49c56e280b43db0c4a5bee722f7ec8ef
seemon9598 <seemon9598@gmail.com>
2023-02-22 23:00:36 +0530

    Mongodb connection

--- server.js
+++ server.js
@@ -1,67 +1,18 @@
-const express = require('express')
-const morgan = require('morgan')
-const mysql = require('mysql2')
+const Mongoose = require("Mongoose");
+const express = require("express");
+const mongoUrl = "http://ec2-65-1-85-153.ap-south-1.compute.amazonaws.com:27017";
+const app=express();
+// Mongoose.set("strictQuery", false);
+Mongoose.set("strictQuery", false);
+// Mongoose.connect(mongoUrl, {useNewUrlParser: true})
+Mongoose.connect("http://ec2-65-1-85-153.ap-south-1.compute.amazonaws.com:27017", { 
+  useNewUrlParser: true
+});
+// const con = Mongoose.connection;
+// con.on('open',function(){
+  console.log('sssssssssss');
+// });
 
-const app = express()
-
-app.use(morgan(":method :url :status :res[content-length] - :response-time ms"))
-
-// https://gist.githubusercontent.com/meech-ward/1723b2df87eae8bb6382828fba649d64/raw/ee52637cc953df669d95bb4ab68ac2ad1a96cd9f/lotr.sql
-const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+app.listen(3000,()=>{
+  console.log("on port 3000!")
 })
-
-function getRandomInt(max) {
-  return 1 + Math.floor(Math.random() * (max-1))
-}
-
-async function getCharacter(id) {
-  const [characters] = await pool.promise().query("SELECT * FROM characters WHERE id = ?", [
-    id,
-  ])
-  return characters[0]
-}
-async function randomId() {
-  const [rows] = await pool.promise().query(
-    "SELECT COUNT(*) as totalCharacters FROM characters"
-  )
-  const { totalCharacters } = rows[0]
-  const randomId = getRandomInt(totalCharacters)
-  return randomId
-}
-
-app.get("/test", (req, res) => {
-  const responseData = {
-    userId:9,
-  userData:{
-      name: "Seemon one",
-      age:24
-  }}
-  res.send(responseData)
-})
-
-app.get("/", async (req, res) => {
-  try {
-    const id = await randomId()
-    const character = await getCharacter(id)
-    res.send(character)
-  } catch (error) {
-    res.send(error)
-  }
-})
-
-app.get("/:id", async (req, res) => {
-  try {
-    const id = parseInt(req.params.id) || await randomId()
-    const character = await getCharacter(id)
-    res.send(character)
-  } catch (error) {
-    res.send(error)
-  }
-})
-
-const port = process.env.PORT || 8080
-app.listen(port, () => console.log(`Listening on port ${port}`))
