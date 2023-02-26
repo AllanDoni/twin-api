f97c13a097229b4abc726be73f71535f586437c1
seemon9598 <seemon9598@gmail.com>
2023-02-26 12:33:42 +0530

    login Api

--- server.js
+++ server.js
@@ -55,6 +55,46 @@ app.post('/register', (req, res) => {
     // res.sendFile(path.join(`${__dirname}/favicon.ico`));
 });
 
+/**
+ * Login 
+ */
+app.post('/login', (req, res) => {
+    Users.findOne({ phoneNumber: req.body.phoneNumber }).then(
+        (user) => {
+          if (!user) {
+            return res.status(401).json({
+              error: new Error('User not found!')
+            });
+          }
+          bcrypt.compare(req.body.password, user.password).then(
+            (valid) => {
+              if (!valid) {
+                return res.status(401).json({
+                  error: new Error('Incorrect password!')
+                });
+              }
+              res.status(200).json({
+                userId: user._id,
+                token: 'token'
+              });
+            }
+          ).catch(
+            (error) => {
+              res.status(500).json({
+                error: error
+              });
+            }
+          );
+        }
+      ).catch(
+        (error) => {
+          res.status(500).json({
+            error: error
+          });
+        }
+      );
+});
+
 app.get('/users', (req, res) => {
     Users.find({}, "name email phoneNumber password").then(members => {
         if (members !== null && members.length > 0) {
