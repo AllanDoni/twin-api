3f2f173661cdc357bc7f1a0ae28dfc30d69fe392
seemon9598 <seemon9598@gmail.com>
2023-02-26 11:59:35 +0530

    chcek

--- server.js
+++ server.js
@@ -38,10 +38,11 @@ app.post('/register', (req, res) => {
           } else {
             const salt = bcrypt.genSalt(10);
             const hash = bcrypt.hash(req.body.password, salt);
+            con
             const tenet = new Users({
                 name : req.body.name,
                 phoneNumber: req.body.phoneNumber,
-                password: hash
+                password: 'hash'
             })
             message = "Registered Successfully";
             const result = tenet.save();
